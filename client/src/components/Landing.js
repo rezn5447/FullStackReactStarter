b6954130f6845f34057b135aa06af126b8f5c90f
Resdan Logan <rezn5447@gmail.com>
2018-02-13 11:28:39 -0800

    update landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Emaily!</h1>
-      Collect feedback from your users
+      <h1>React App Starter!</h1>
+      <p>Collect feedback from your users using stripe Auth</p>
+      <p>Using passportJS and google</p>
     </div>
   );
 };
